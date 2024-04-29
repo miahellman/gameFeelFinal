3e89e73e24c06837b848162d67f3eec1e731429c
Chas-w <75660257+Chas-w@users.noreply.github.com>
2024-04-29 15:14:28 -0400

    Obstacle Spawning is in and working but there's something weird going on with the barriers?

--- Assets/Scripts/Road Spawner Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/Road Spawner Scripts/ObstacleSpawner.cs
@@ -4,8 +4,90 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
- //empty script to spawn random obstacles per track segment
- //this script is attached to the road prefab so when a new road spawns, this script can spawn obstacles on the road
- //it wasn't working so i deleted the script and am now researching new solutions
+    [Header("Position Data")]
+    public GameObject maxX;
+    public GameObject minX;
+    public GameObject cam;
+
+    [Header("Obstacles")]
+    public GameObject obstacle1; 
+    public GameObject obstacle2;
+    public GameObject obstacle3;
+    public GameObject obstacle4;
+    public GameObject spawn; 
+
+    [Header("Timing Data")]
+    [SerializeField] float timerMax;
+    [SerializeField] float timerMin;
+    [SerializeField] float timer; 
+
+    private void Start()
+    {
+        cam = GameObject.Find("Camera");
+        maxX = GameObject.Find("ObstacleSpawnerMax");
+        minX = GameObject.Find("ObstacleSpawnerMin");
+    }
+
+    private void Update()
+    {
+        SetZPos();
+
+        if (timer <= 0)
+        {
+            timer = 0;
+            Spawning();
+            //Timing(); 
+        }
+
+        if (timer > 0)
+        {
+            timer--;
+        }
+    }
+
+    private void SetZPos()
+    {
+        float// positionZ = transform.position.z;
+        positionZ = cam.transform.position.z;
+        transform.position = new Vector3(transform.position.x, transform.position.y, positionZ);
+    }
+
+    private void Timing()
+    {
+
+        //Spawning(); 
+        //timer = (Random.Range(timerMin, timerMax));
+    }
+
+    private void Spawning()
+    {
+        #region random obstacle to spawn
+        int spawnChoice = Random.Range(0, 3);
+
+        if (spawnChoice == 0)
+        {
+            spawn = obstacle1;
+        }
+        if (spawnChoice == 1)
+        {
+            spawn = obstacle2;
+        }
+        if (spawnChoice == 2)
+        {
+            spawn = obstacle3;
+        }
+        if (spawnChoice == 3)
+        {
+            spawn = obstacle4;
+        }
+        #endregion
+
+        float spawnPosX = Random.Range(minX.transform.position.x, maxX.transform.position.x); 
+
+        Instantiate(spawn, new Vector3(spawnPosX, maxX.transform.position.y, maxX.transform.position.z), Quaternion.identity);
+
+        timer = (Random.Range(timerMin, timerMax));
+
+    }
 }
 
